ae300af05cfdc144aec2e71d666bbdf90925ea30
Anirban Roy <aroyiem@gmail.com>
2024-10-29 12:30:26 +0000

    State handling, data props, bind events between parent and child component, extract data from form

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import CourseGoal from "./components/CourseGoal.tsx";
+import goalsImg from "./assets/goals.jpg";
+import Header from "./components/Header.tsx";
+import {useState} from "react";
+import {CourseGoalList} from "./components/CourseGoalList.tsx";
+import NewGoal from "./components/NewGoal.tsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+export type CourseGoal = {
+    title: string;
+    description: string;
+    id: number;
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+
+    const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+    function handleAddGoal(goal: string, summary: string) {
+        // ...
+        setGoals(prevGoals => {
+            const newGoal: CourseGoal = {
+                id: Math.random(),
+                title: goal,
+                description: summary
+            };
+            return [...prevGoals, newGoal]
+        });
+    }
+
+    function handleDeleteGoal(id: number) {
+        // ...
+        setGoals(prevGoals =>
+            prevGoals.filter(goal => goal.id !== id));
+    }
+
+    return (
+        <main>
+            <Header image={{src: goalsImg, alt: 'A list of goals'}}>
+                <h1>Your course goals</h1>
+            </Header>
+            <NewGoal onAddGoal={handleAddGoal}/>
+            <CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal}/>
+        </main>
+    );
 }
-
-export default App
---
+++ src/components/CourseGoalList.tsx
@@ -0,0 +1,21 @@
+import CourseGoal from "./CourseGoal.tsx";
+import {type CourseGoal as CGoal} from "../App.tsx";
+
+type CourseGoalListProps = {
+    goals: CGoal[];
+    onDeleteGoal: (id: number) => void;
+};
+
+export function CourseGoalList({goals, onDeleteGoal}: CourseGoalListProps) {
+    return (
+        <ul>
+            {goals.map((goal) => (
+                <li key={goal.id}>
+                    <CourseGoal title={goal.title} onDelete={onDeleteGoal} id={goal.id}>
+                        <p>{goal.description}</p>
+                    </CourseGoal>
+                </li>
+            ))}
+        </ul>
+    );
+}
